0ff6747719ebd1711c17e60d93fbe8577498ee96
mmcguire91 <mmcguire91@gmail.com>
2020-05-08 14:01:16 -0400

    Completed xylophone project

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,82 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
-              },
-              child: Text('data'),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  playSound(1);
+                },
+                color: Colors.red,
+                child: null,
+              ),
             ),
-          ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  playSound(2);
+                },
+                color: Colors.orange,
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  playSound(3);
+                },
+                color: Colors.yellow,
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  playSound(4);
+                },
+                color: Colors.green,
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  playSound(5);
+                },
+                color: Colors.teal,
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  playSound(6);
+                },
+                color: Colors.blue,
+                child: null,
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  playSound(7);
+                },
+                color: Colors.purple,
+                child: null,
+              ),
+            ),
+          ],
         ),
       ),
     );
